3c4967c1285163fc7662a4a3052c40c862f65b41
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-29 20:49:08 +0000

    Refactored the setupInput function and callback to an input.js file, exported and then required it within play.js

--- play.js
+++ play.js
@@ -1,22 +1,8 @@
 const {connect} = require('./client');
+const { setupInput } = require('./input');
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-// We expect key strokes to be input to this function. 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
