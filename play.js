15712825d4fcc964462985ef3d4f18daba42e0c2
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-29 20:11:49 +0000

    Added a client.js file which hosts the net and connect functions. Imported the connect function to play.js for future use and tested to ensure everything works as before

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:"10.0.2.15", // IP address here,
-    port:'50541' // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => console.log(`${data}`)); // Or console.log(data.toString())
-
-
-  return conn;
-};
+const {connect} = require('./client');
 
 console.log("Connecting ...");
 connect();
