d47ba38e7fe1a10873a4dab2b78511162f395375
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-29 21:26:33 +0000

    Added the ability to read keyboard strokes on the server side by use of setting a global connection object in the input.js file and setting up setupInput to take in a conn object (i.e. the return from connect(). Updated play.js to take in this return. Updated input.js handleUserInput function to use connection.write() to send movement strings to the server through the connection object.

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,9 +14,22 @@ const setupInput = function () {
 
 // We expect key strokes to be input to this function. 
 const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
+    switch (key){
+      case '\u0003':
+        process.exit();
+      case 'w':
+        connection.write(`Move: up`);
+        break;
+      case 'a':
+        connection.write(`Move: left`);
+        break;
+      case 's':
+        connection.write(`Move: down`);
+        break;
+      case 'd':
+        connection.write(`Move: right`);
+        break;
+    };
 };
 
 module.exports = {
