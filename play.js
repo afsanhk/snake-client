f6d37ba3cfb7a3e7ad0f851e45c8b6132982e97d
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-01 01:42:12 +0000

    Added some comments

--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 const { connect } = require('./client');
 const { setupInput } = require('./input');
 
+
 console.log("Connecting ...");
 setupInput(connect()); //Setup input takes the returned object from connect() as an input, so that the stdin keystrokes can be communicated to the server!
